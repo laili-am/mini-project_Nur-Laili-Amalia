8939c18e71dc16ce051b57af7c88822e1ba42008
Laili <amalialaili31@gmail.com>
2022-05-18 19:40:15 +0700

    push miniproject 2

--- src/App.js
+++ src/App.js
@@ -1,30 +1,28 @@
-import React from 'react';
-import './App.css';
-import { BrowserRouter,Routes,Route } from "react-router-dom";
-import About from './pages/About/about';
-import Home from './pages/Home/home';
-import AddRecipe from './pages/AddRecipe/form';
-import NotFound from './pages/NotFound/notfound';
-import MyRecipe from './pages/MyRecipe/myrecipe';
-import Navbar from './components/Navbar/Navbar'
-import FormRecipe from './pages/MyRecipe/formmyrecipe'
-import EditRecipe from './pages/EditRecipe/editrecipe';
-
+import React from "react";
+import "./App.css";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import About from "./pages/About/about";
+import Home from "./pages/Home/home";
+import AddRecipe from "./pages/AddRecipe/form";
+import Navbar from "./components/Navbar/Navbar";
+import FormRecipe from "./pages/MyRecipe/formmyrecipe";
+import EditRecipe from "./pages/EditRecipe/editrecipe";
+import NotFound from "./pages/NotFound/notfound";
 function App() {
   return (
     <>
-    <BrowserRouter>
-      <Navbar/>
-      <Routes>
-        <Route path='/' element={<Home/>} />
-        <Route path='/home' element={<Home/>} />
-        <Route path='/about' element={<About/>} />
-        <Route path='/addrecipe' element={<AddRecipe/>} />
-        <Route path='/myrecipe' element={<FormRecipe/>} />
-        <Route path='/editrecipe/:id' element={<EditRecipe/>}/>
-        <Route path="*" element={<NotFound/>}/>
-      </Routes>
-    </BrowserRouter>
+      <BrowserRouter>
+        <Navbar />
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/home" element={<Home />} />
+          <Route path="/about" element={<About />} />
+          <Route path="/addrecipe" element={<AddRecipe />} />
+          <Route path="/myrecipe" element={<FormRecipe />} />
+          <Route path="/editrecipe/:id" element={<EditRecipe />} />
+          <Route path="*" element={<NotFound />} />
+        </Routes>
+      </BrowserRouter>
     </>
   );
 }
